7848224b1ded2169e74561cd6b5ccf1c488a0993
ActiveZ <activez.git@gmail.com>
2021-12-20 18:16:52 +0100

    tp 3 ok

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -3,13 +3,19 @@ package dev;
 import com.github.lalyos.jfiglet.FigletFont;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) throws IOException {
-        String txt = FigletFont.convertOneLine("Hello jFiglet!");
+        String titre = ResourceBundle.getBundle("application").getString("titre");
+        String txt = FigletFont.convertOneLine(titre);
         System.out.println(txt);
+
+        // affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environnement : " + environnement);
     }
 }
