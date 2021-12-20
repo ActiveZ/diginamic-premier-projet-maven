dcb7f924e5133840571e471cca9da2a9b7d80987
ActiveZ <activez.git@gmail.com>
2021-12-20 17:03:04 +0100

    tp 1 hello figlet ok

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,15 @@
 package dev;
 
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws IOException {
+        String txt = FigletFont.convertOneLine("Hello jFiglet!");
+        System.out.println(txt);
     }
 }
